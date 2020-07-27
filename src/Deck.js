48604ce0aa098063f3c42f7336e312d9bbce3702
Shreesh Nayak <shreeshnayak@gmail.com>
2020-07-27 15:44:11 -0700

    Added panresponder

--- src/Deck.js
+++ src/Deck.js
@@ -1,8 +1,28 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { StyleSheet, Text, View, PanResponder, Animated } from 'react-native'
 
 
 const Deck = ({ data, renderCard }) => {
+    
+    const position = useRef(new Animated.ValueXY()).current
+
+    const panResponder = PanResponder.create({
+
+    // executed any time a user presses down on the screen
+    // if we return true, then this panResponder should be
+    // responsible for handling that gesture
+    onStartShouldSetPanResponder: () => true,
+    // a true callback. It is called anytime the user
+    // starts to drag their finger around the screen
+    onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy })
+    },
+    // It is called anytime a user lets go of the screen
+    // after moving something on the screen
+    onPanResponderRelease: () => {}
+    })
+
+    const [pResonder, setPResponder] = useState(panResponder)
     
     const renderCards = () => {
         return data.map(item => {
@@ -11,9 +31,9 @@ const Deck = ({ data, renderCard }) => {
     }
     
     return (
-        <View>
+        <Animated.View {...pResonder.panHandlers} style={position.getLayout()}>
             {renderCards()}
-        </View>
+        </Animated.View>
     )
 }
 
